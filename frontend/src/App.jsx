e5191cfa6ba7beb0dc4037f41af0559f3c94398d
kdb04 <nikhil.srivatsa1@gmail.com>
2024-10-17 22:44:59 +0530

    about page done

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "./HomePage";
 import LoginForm from "./Login";
 import Booking from "./Booking"
+import About from "./About"
 
 function App(){
     return(
@@ -12,6 +13,7 @@ function App(){
                     <Route path="/" element={<HomePage />}/>
                     <Route path="/Login" element={<LoginForm />}/>
                     <Route path="/Booking" element={<Booking />}/>
+                    <Route path="/About" element={<About />}/>
                 </Routes>
             </div>
         </Router>
