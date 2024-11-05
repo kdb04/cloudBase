162ad53318e742843d99f753c23e677b2bd7bb66
kdb04 <nikhil.srivatsa1@gmail.com>
2024-11-05 16:17:15 +0530

    edit coz of db change

--- backend/src/controllers/bookingController.js
+++ backend/src/controllers/bookingController.js
@@ -1,11 +1,11 @@
 const db = require("../connection/db.js");
 
 const bookTicket = (req, res) => {
-    const { passenger_no, class: travelClass, food_preference, date, source, destination, seat_no, flight_id } = req.body;
+    const { passenger_no, class: travelClass, food_preference,  source, destination, seat_no, flight_id } = req.body;
     //const query = `DESC Ticket`;
-    const query = `INSERT INTO Ticket (passenger_no, class, food_preference, date, source, destination, seat_no, flight_id) VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    const query = `INSERT INTO ticket (passenger_no, class, food_preference, source, destination, seat_no, flight_id) VALUES (?, ?, ?, ?, ?, ?, ?)`;
 
-    db.query(query, [passenger_no, travelClass, food_preference, date, source, destination, seat_no, flight_id], (err, results) => {
+    db.query(query, [passenger_no, travelClass, food_preference, source, destination, seat_no, flight_id], (err, results) => {
         if (err){
             console.error(err);
             return res.status(500).json({ message: "Error booking flight", error: err});
