97ba2e6f3cd2c57c9c0dcd9cdffd0b19f9b8bab0
kdb04 <nikhil.srivatsa1@gmail.com>
2024-11-06 19:22:55 +0530

    updated sql triggers and functions

---
+++ database/functions.sql
@@ -0,0 +1,31 @@
+CREATE DEFINER=`root`@`localhost` PROCEDURE `alternative`(IN cancelled_flight_id BIGINT, IN departure_date date)
+BEGIN
+declare cancelled_destination varchar(50);
+declare cancelled_source varchar(50);
+declare alt_price bigint;
+select source, destination, price into cancelled_source, cancelled_destination, alt_price from Flights where flight_id=cancelled_flight_id;
+select flight_id, airline_id , departure,arrival ,available_seats, price from Flights where source=cancelled_source and destination=cancelled_destination and available_seats>0 and flight_id!=cancelled_flight_id and status!='canceled' and status!="air"
+order by price asc;
+END;
+// 
+
+CREATE DEFINER=`root`@`localhost` PROCEDURE `dynamic_pricing`(flightid bigint)
+BEGIN
+declare base bigint;
+declare seats bigint;
+declare new_price bigint;
+select price, available_seats into base,seats from Flights where flightid=flight_id;
+
+if seats<=5 then
+	set new_price=base*1.20;
+
+elseif seats<=10 then
+	set new_price=base*1.10;
+else
+	set new_price=base;
+END if;
+
+update Flights set price=new_price where flightid=flight_id;
+END;
+// 
+
--- database/triggers.sql
+++ database/triggers.sql
@@ -25,4 +25,28 @@ BEGIN
    END IF;
 END$$
 
+-- Trigger3 -> solves runway conflicts
+DELIMITER //
+CREATE TRIGGER prevent_runway_conflict
+BEFORE INSERT ON Flights
+FOR EACH ROW
+BEGIN
+    DECLARE conflict_count INT;
+
+
+    SELECT COUNT(*) INTO conflict_count
+    FROM Flights
+    WHERE runway_no = NEW.runway_no
+      AND date = NEW.date
+      AND flight_id <> NEW.flight_id
+      AND ABS(TIMESTAMPDIFF(MINUTE, NEW.departure, departure)) < 30;
+
+
+    IF conflict_count > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Schedule conflict: Another flight is assigned to the same runway within 30 minutes.';
+    END IF;
+END;
+//
+
 DELIMITER ;
